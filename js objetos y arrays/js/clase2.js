d09ab628f1c68d8599bdd1237b2a27e8d7d02aaa
webtarifa <79761867+webtarifa@users.noreply.github.com>
2021-07-30 18:42:56 +0200

    simuladorNotas

---
+++ js objetos y arrays/js/clase2.js
@@ -0,0 +1,37 @@
+function capturar(){
+    console.log("capturado");
+    function Persona(nombre,asignatura,nota1,nota2,nota3,notamedia){
+        this.nombre=nombre;
+        this.asignatura=asignatura;
+        this.nota1=nota1;
+        this.nota2=nota2;
+        this.nota3=nota3;
+        this.notamedia=notamedia;
+        
+    }
+    let nombreCapturar = document.getElementById("nombre").value;
+    console.log(nombreCapturar);
+    let asignaturaCapturar = document.getElementById("asignatura").value;
+    console.log(asignaturaCapturar);
+    let nota1Capturar = parseInt;
+    let nota1Capturar = document.getElementById("nota1").value;
+    console.log(nota1Capturar);
+    let nota2Capturar = parseInt;
+    let nota2Capturar = document.getElementById("nota2").value;
+    console.log(nota2Capturar);
+    let nota3Capturar = parseInt;
+    let nota3Capturar =  document.getElementById("nota3").value;
+    console.log(nota3Capturar);
+    
+    let = notamedia = nota1Capturar + nota2Capturar+ nota3Capturar /3;
+    nuevoSujeto = new Persona(nombreCapturar,asignaturaCapturar,nota1Capturar,nota2Capturar,nota3Capturar,notamedia );
+    console.log(nuevoSujeto);
+    agregar();
+}
+
+let baseDatos= [];
+function agregar(){
+    baseDatos.push(nuevoSujeto);
+    console.log(baseDatos);
+    document.getElementById("tabla").innerHTML += "<tbody><td>"+nuevoSujeto.nombre+"</td><td>"+nuevoSujeto.asignatura+"</td><td>"+nuevoSujeto.nota1+"</td><td>"+nuevoSujeto.nota2+"</td><td>"+nuevoSujeto.nota3+"</td><td>"+nuevoSujeto.notamedia+"</td></tbody>";
+}
