c6fe5402cf322a39fcc186465ec79e1c1b829309
webtarifa <79761867+webtarifa@users.noreply.github.com>
2021-08-03 23:45:53 +0200

    uoc

---
+++ storage.js
@@ -0,0 +1,20 @@
+
+localStorage.setItem("nombreCapturar",JSON.stringify(nombreCapturar));
+
+/*function guardar_localStorage(){
+    let persona = {
+        nombre:"nombreCapturar" ,
+        asignatura:"()",
+        nota1:"()",
+        nota2:"()",
+        nota3:"()",
+        notamedia:"()"
+    }
+}
+let persona="";
+localStorage.setItem("persona", JSON.stringify(baseDatos));
+
+
+
+
+
